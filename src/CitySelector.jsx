bf21924076d539de87113f2f0e52ad5a3d9a098c
KareimGazer <kareimgazar1990@gmail.com>
2024-09-01 06:19:43 +0300

    used states instead of cities

--- src/CitySelector.jsx
+++ src/CitySelector.jsx
@@ -1,18 +1,17 @@
 import worldCities from "./worldCities"
 
 const CitySelector = ({ selectedCity, setSelectedCity, selectedCountry }) => {
-    const citesList = worldCities[selectedCountry]
-
+    const citesList = worldCities.filter((city) => city["country_name"] === selectedCountry)
     return (
         <select
             className="select select-bordered w-full max-w-xs"
             value={selectedCity === "" ? "Select a city" : selectedCity}
             onChange={(e) => setSelectedCity(e.target.value)}
         >
-            <option value="" disabled>Select a city</option>
+            <option value="">Select a city</option>
             {citesList.map((city) => (
-                <option key={city} value={city}>
-                    {city}
+                <option key={city.id} value={city.name}>
+                    {city.name}
                 </option>
             ))}
         </select>
